eb5c37e9d0dd56d172e4d52b77ca4f05a5d65542
Chris Blazek <chris.blazek@gmail.com>
2018-07-24 15:50:24 -0500

    replacing colors that were stripped from a previous d3-scale library

--- src/commonProps.jsx
+++ src/commonProps.jsx
@@ -23,7 +23,8 @@ export const pieProps = {
   height: height,
   margins: margins,
   innerRadius: 0,
-  categoricalColors: D3Scale.scaleCategory10(),
+//  categoricalColors: D3Scale.scaleCategory10(),
+  categoricalColors: ["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf"],
   pieSort: () => {},
   pieTextShow: true
 }
